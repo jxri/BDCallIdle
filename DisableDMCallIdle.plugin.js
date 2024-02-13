74b6805a258a5b1efc43e786e1d166ac55e30395
jerx <39219750+jxri@users.noreply.github.com>
2024-02-13 10:12:17 -0500

    Update DisableDMCallIdle.plugin.js

--- DisableDMCallIdle.plugin.js
+++ DisableDMCallIdle.plugin.js
@@ -2,175 +2,165 @@
  * @name DisableDMCallIdle
  * @author jer
  * @authorId 728023524240785420
- * @version 1.0.0
+ * @version 1.0.1
  * @description Disables automatically getting kicked from a DM voice call after 3 minutes.
  * @source https://github.com/jxri/BDCallIdle
  * @updateUrl https://jxri.github.io/BDCallIdle/DisableDMCallIdle.plugin.js
  */
+
 /*@cc_on
 @if (@_jscript)
-var shell = WScript.CreateObject("WScript.Shell");
-var fs = new ActiveXObject("Scripting.FileSystemObject");
-var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-var pathSelf = WScript.ScriptFullName;
-shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-} else if (!fs.FolderExists(pathPlugins)) {
-shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-} else if (shell.Popup("Should I move myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-fs.MoveFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)));
-shell.Exec("explorer " + pathPlugins);
-shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-}
-WScript.Quit();
+
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
 @else@*/
+
 module.exports = (() => {
-  const config = {
-    info: {
-      name: "DisableDMCallIdle",
-      authors: [
-        {
-          name: "jer",
-          discord_id: "728023524240785420",
-          github_username: "jxri",
-        },
-      ],
-      version: "1.0.0",
-      description:
-        "Disables automatically getting kicked from a DM voice call after 3 minutes.",
-      github: "https://github.com/jxri/BDCallIdle",
-      github_raw:
-        "https://jxri.github.io/BDCallIdle/DisableDMCallIdle.plugin.js",
-    },
-    changelog: [
-      {
-        title: "v1.0.0",
-        items: [
-          "This is the initial release of the plugin :)",
-          "I :3 wannya *looks at you* cuddwe w-w-with my fiancee :3 (p≧w≦q)",
-        ],
-      }
-    ],
-    main: "DisableDMCallIdle.plugin.js",
-  };
-  const RequiredLibs = [{
-    window: "ZeresPluginLibrary",
-    filename: "0PluginLibrary.plugin.js",
-    external: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-    downloadUrl: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js"
-  },
-  {
-    window: "BunnyLib",
-    filename: "1BunnyLib.plugin.js",
-    external: "https://github.com/Tharki-God/BetterDiscordPlugins",
-    downloadUrl: "https://tharki-god.github.io/BetterDiscordPlugins/1BunnyLib.plugin.js"
-  },
-  ];
-  class handleMissingLibrarys {
-    load() {
-      for (const Lib of RequiredLibs.filter(lib => !window.hasOwnProperty(lib.window)))
-        BdApi.showConfirmationModal(
-          "Library Missing",
-          `The library plugin (${Lib.window}) needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+    const config = {
+      info: {
+        name: "DisableDMCallIdle",
+        authors: [
           {
-            confirmText: "Download Now",
-            cancelText: "Cancel",
-            onConfirm: () => this.downloadLib(Lib),
-          }
-        );
-    }
-    async downloadLib(Lib) {
-      const fs = require("fs");
-      const path = require("path");
-      const { Plugins } = BdApi;
-      const LibFetch = await fetch(
-        Lib.downloadUrl
-      );
-      if (!LibFetch.ok) return this.errorDownloadLib(Lib);
-      const LibContent = await LibFetch.text();
-      try {
-        await fs.writeFile(
-          path.join(Plugins.folder, Lib.filename),
-          LibContent,
-          (err) => {
-            if (err) return this.errorDownloadLib(Lib);
-          }
-        );
-      } catch (err) {
-        return this.errorDownloadLib(Lib);
-      }
-    }
-    errorDownloadZLib(Lib) {
-      const { shell } = require("electron");
-      BdApi.showConfirmationModal(
-        "Error Downloading",
-        [
-          `${Lib.window} download failed. Manually install plugin library from the link below.`,
-        ],
-        {
-          confirmText: "Download",
-          cancelText: "Cancel",
-          onConfirm: () => {
-            shell.openExternal(
-              Lib.external
-            );
+            name: "jer",
+            discord_id: "728023524240785420",
+            github_username: "jxri",
           },
-        }
-      );
-    }
-    start() { }
-    stop() { }
-  }
-  return RequiredLibs.some(m => !window.hasOwnProperty(m.window))
-    ? handleMissingLibrarys
-    : (([Plugin, ZLibrary]) => {
-      const {
-        Logger,
-        PluginUpdater,
-        Patcher
-      } = ZLibrary;
-
-      const {
-        LibraryModules: {
-          Timeout,      
+        ],
+        version: "1.0.1",
+        description:
+          "Disables automatically getting kicked from a DM voice call after 3 minutes.",
+        github: "https://github.com/jxri/BDCallIdle",
+        github_raw:
+          "https://jxri.github.io/BDCallIdle/DisableDMCallIdle.plugin.js",
+      },
+      changelog: [
+        {
+          title: "v1.0.1",
+          items: [
+            "Removal of 1BunnyLib library",
+          ],
         },
-      } = BunnyLib.build(config);
+        {
+          title: "v1.0.0",
+          items: [
+            "This is the initial release of the plugin :)",
+            "I :3 wannya *looks at you* cuddwe w-w-with my fiancee :3 (p≧w≦q)",
+          ],
+        }
+      ],
+      main: "DisableDMCallIdle.plugin.js",
+    };
 
-      return class DisableDMCallIdle extends Plugin {
+    return !global.ZeresPluginLibrary ? class {
         constructor() {
-          super();
+           this.start = this.load = this.handleMissingLib;
         }
-
-        checkForUpdates() {
-          try {
-            PluginUpdater.checkForUpdate(
-              config.info.name,
-              config.info.version,
-              config.info.github_raw
-            );
-          } catch (err) {
-            Logger.err("Plugin Updater could not be reached.", err);
-          }
+  
+        getName() {
+           return config.info.name.replace(/\s+/g, '');
         }
-
-        onStart() {
-          this.checkForUpdates();
-          this.initialize();
+  
+        getAuthor() {
+           return config.info.authors.map(a => a.name).join(', ');
         }
-
-        initialize() {
-          Patcher.after(Timeout.prototype, "start", (timeout, [_, args]) => {
-            if (args?.toString().includes("BOT_CALL_IDLE_DISCONNECT")) {
-              timeout.stop();
-            }
-          });   
+  
+        getVersion() {
+           return config.info.version;
         }
-
-        onStop() {
-          Patcher.unpatchAll();
+  
+        getDescription() {
+           return config.info.description + ' You are missing libraries for this plugin, please enable the plugin and click Download Now.';
         }
-      };
-    })(ZLibrary.buildPlugin(config));
-})();
-/*@end@*/
+  
+        start() { } 
+        stop() { }
+  
+        async handleMissingLib() {
+           const request = require('request');
+           const path = require('path');
+           const fs = require('fs');
+  
+           const dependencies = [
+              {
+                 global: 'ZeresPluginLibrary',
+                 filename: '0PluginLibrary.plugin.js',
+                 external: 'https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js',
+                 url: 'https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js'
+              }
+           ];
+  
+           if (!dependencies.map(d => window.hasOwnProperty(d.global)).includes(false)) return;
+  
+           if (global.eternalModal) {
+              while (global.eternalModal && dependencies.map(d => window.hasOwnProperty(d.global)).includes(false)) await new Promise(f => setTimeout(f, 1000));
+              if (!dependencies.map(d => window.hasOwnProperty(d.global)).includes(false)) return BdApi.Plugins.reload(this.getName());
+           };
+  
+           global.eternalModal = true;
+  
+           BdApi.showConfirmationModal(
+              'Dependencies needed',
+              `Dependencies needed for ${this.getName()} are missing. Please click download to install the dependecies.`,
+              {
+                 confirmText: 'Download',
+                 cancelText: 'Cancel',
+                 onCancel: () => delete global.eternalModal,
+                 onConfirm: async () => {
+                    for (const dependency of dependencies) {
+                       if (!window.hasOwnProperty(dependency.global)) {
+                          await new Promise((resolve) => {
+                             request.get(dependency.url, (error, res, body) => {
+                                if (error) return electron.shell.openExternal(dependency.external);
+                                fs.writeFile(path.join(BdApi.Plugins.folder, dependency.filename), body, resolve);
+                             });
+                          });
+                       }
+                    }
+  
+                    delete global.eternalModal;
+                 }
+              }
+           );
+        }
+     } : (([Plugin, API]) => {
+        const { Patcher, WebpackModules } = API;
+        const { Timeout } = WebpackModules.getByProps('Timeout');
+  
+        return class extends Plugin {
+           constructor() {
+              super();
+           }
+  
+           start() {
+              Patcher.after(Timeout.prototype, 'start', (instance, args) => {
+                 if (args[1]?.toString().includes('BOT_CALL_IDLE_DISCONNECT')) {
+                    instance.stop();
+                 };
+              });
+           };
+  
+           stop() {
+              Patcher.unpatchAll();
+           };
+        };
+     })(ZLibrary.buildPlugin(config));
+  })();
+  
+  /*@end@*/
